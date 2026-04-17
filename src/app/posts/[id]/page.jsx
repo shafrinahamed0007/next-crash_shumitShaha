1ff8656c27355eb21eeedde39cb975c8fb5b61f9
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-04-17 18:23:00 +0600

    Dynamically declare with single post meta data

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -2,6 +2,16 @@
 import { notFound } from "next/navigation";
 import getPost from "../../../../lib/getPost";
 
+export async function generateMetadata({ params }) {
+  const { id } = await params;
+  const post = await getPost(id);
+
+  return {
+    title: post.title,
+    description: post.body,
+  };
+}
+
 export default async function PostPage({ params }) {
   const { id } = await params;
 
