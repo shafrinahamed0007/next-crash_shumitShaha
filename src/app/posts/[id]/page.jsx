5e828e362c57f19cef24f1145f69ef9177eb8194
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-04-17 20:19:21 +0600

    Declare comments and post dynamically :)

--- src/app/posts/[id]/page.jsx
+++ src/app/posts/[id]/page.jsx
@@ -1,6 +1,7 @@
 // import React from "react";
 import { notFound } from "next/navigation";
 import getPost from "../../../../lib/getPost";
+import getComments from "../../../../lib/getPostComment";
 
 export async function generateMetadata({ params }) {
   const { id } = await params;
@@ -15,7 +16,10 @@ export async function generateMetadata({ params }) {
 export default async function PostPage({ params }) {
   const { id } = await params;
 
-  const post = await getPost(id);
+  const postPromise = await getPost(id);
+  const commentPromise = await getComments(id);
+
+  const [post, comments] = await Promise.all([postPromise, commentPromise]);
 
   if (!post || !post.id) {
     return notFound();
@@ -26,6 +30,15 @@ export default async function PostPage({ params }) {
     <div className="mt-6 container mx-auto">
       <h2 className="text-blue-500 text-xl font-semibold">{post?.title}</h2>
       <p className="mt-3">{post?.body}</p>
+      <hr />
+      <div className="mt-10">
+        <h2>Comments</h2>
+        <ul>
+          {
+            comments.map(comment => <li className="mb-2" key={comment.id}>{comment?.body}</li>)
+          }
+        </ul>
+      </div>
     </div>
   );
 }
